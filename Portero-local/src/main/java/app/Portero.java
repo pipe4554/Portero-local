01baddd77a897fbe27058b58f9530e8eebac2c93
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-14 13:29:48 +0100

    Update Portero.java

--- Portero-local/src/main/java/app/Portero.java
+++ Portero-local/src/main/java/app/Portero.java
@@ -1,8 +1,53 @@
 package app;
 
+import java.util.ArrayList;
+
 import java.util.concurrent.Semaphore;
 
 public class Portero {
-	private int Aforo_Maximo = Edificio.AFORO_MAX; //Obtiene el aforo maximo del edificio el cual protege, asi el portero puede proteger distintos  edificios
+	private static final int MAX_AVAILABLE = Edificio.getAFORO_MAX(); //tamaño maximo del aforo
+	private final Semaphore available = new Semaphore(MAX_AVAILABLE, true); // el semaforo
+
 	
+	protected ArrayList<Persona> Aforo = new ArrayList<Persona>();
+
+	protected boolean[] used = new boolean[MAX_AVAILABLE];
+
+	public Persona getItem() throws InterruptedException {
+		//el hilo "pide turno"
+		available.acquire();
+		return getNextAvailableItem();
+	}
+
+	public void putItem(Persona persona) throws InterruptedException {
+		//el hilo "pide turno"
+		available.acquire();
+		if (markAsUnused(persona)) {
+			available.release();
+		}
+	}
+	
+	
+	protected synchronized Persona getNextAvailableItem() {
+		for (int i = 0; i < MAX_AVAILABLE; ++i) {
+			if (!used[i]) {
+				used[i] = true;
+				return Aforo.get(i);
+			}
+		}
+		Persona persona = new Persona(null);
+		return persona; // not reached
+	}
+
+	protected synchronized boolean markAsUnused(Persona persona) {
+		for (int i = 0; i < MAX_AVAILABLE; ++i) {
+			if (!used[i]) {
+				Aforo.set(i, persona);
+				return true;
+			}
+
+		}
+		return false;
+
+	}
 }
