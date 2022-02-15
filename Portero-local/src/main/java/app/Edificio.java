579078ee2ae06499dd7dc562a59ba588f113f75f
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-15 19:14:55 +0100

    Update Edificio.java

--- Portero-local/src/main/java/app/Edificio.java
+++ Portero-local/src/main/java/app/Edificio.java
@@ -6,12 +6,9 @@ import java.util.List;
 public class Edificio {
 	private static final int AFORO_MAX=31; // Aforo maximo del local el cual es invariable y siempre es 31, por lo tanto no hace falta protegerlo
 	private List<Persona> Aforo = new ArrayList<Persona>(); // representacion fisica del aforo actual el cual guarda personas  
-	private int Aforo_actual=Aforo.size(); //representacion numerica del aforo actual, sirve solo para representar ese valor y no altera ninguna otra operación
-	private static int Aforo_Total=0; //variable estatica del aforo total al final del día/programa, se sumara en la operación de Entrada() y la compartira toda la clase
+	private int Aforo_actual=Aforo.size(); //representacion numerica del aforo actual, sirve solo para representar ese valor y no altera ninguna otra operaciÃ³n
+	private static int Aforo_Total=0; //variable estatica del aforo total al final del dÃ­a/programa, se sumara en la operaciÃ³n de Entrada() y la compartira toda la clase
 	
-	public void Edificio(List<Persona> Aforo){
-		this.Aforo = Aforo;
-	}
 	
 	public int getAforo_Actual() {
 		return Aforo_actual;
@@ -22,5 +19,13 @@ public class Edificio {
 	
 	public void EntraPersona() {
 		Aforo_Total+=1;
+	}
+	
+	public Persona getPersona(int i) {
+		return Aforo.get(i);
+	}
+	
+	public Persona setPersona(int i, Persona persona) {
+		return Aforo.set(i, persona);
 	}
 }
