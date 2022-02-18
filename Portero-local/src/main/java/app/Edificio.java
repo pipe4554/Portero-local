bdbbb89a47f6b629ae368d8cfba6da881f41cf84
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-18 11:05:52 +0100

    Update Edificio.java

--- Portero-local/src/main/java/app/Edificio.java
+++ Portero-local/src/main/java/app/Edificio.java
@@ -7,8 +7,12 @@ public class Edificio {
 	private static final int AFORO_MAX=31; // Aforo maximo del local el cual es invariable y siempre es 31, por lo tanto no hace falta protegerlo
 	private List<Persona> Aforo = new ArrayList<Persona>(); // representacion fisica del aforo actual el cual guarda personas  
 	private int Aforo_actual=0; //representacion numerica del aforo actual, sirve solo para representar ese valor y no altera ninguna otra operaciÃ³n
-	private static int Aforo_Total=0; //variable estatica del aforo total al final del dÃ­a/programa, se sumara en la operaciÃ³n de Entrada() y la compartira toda la clase
-	
+	/*
+	 * variable estatica del aforo total al final del dÃ­a/programa, se sumara en la operaciÃ³n de Entrada()
+	 *  y la compartira toda la clase, puede ser una variable no estatica para tratarlo de manera mas simple,
+	 *   pero a la vez puede producir errores a la hora del recuento
+	 */
+	private static int Aforo_Total=0; 
 	
 	public int getAforo_Actual() {
 		return Aforo_actual;
@@ -17,29 +21,42 @@ public class Edificio {
 		return AFORO_MAX;
 	}
 	
-	//-------------------------------------------Suma del Aforo total final
+	/*
+	 * metodos para el recuento actual y total del aforo
+	 */
+	
+	/**
+	 * metodo para la entrada de personas, al sumar el aforo actual tambien se suma el aforo total
+	 */
 	public void entra() {
 		Aforo_actual+=1;
 		Aforo_Total+=1;
 	}
+	/**
+	 * metodo para la salida de personas 
+	 */
 	public void sale() {
 		Aforo_actual-=1;
 	}
 	
 	
 	
-	//-------------------------------------------
+	/*
+	 * Metodos de entrada y salida de personas en el Array de personas
+	 */
 	public Persona getPersona(int i) {
 		Aforo.remove(i);
 		return Aforo.get(i);
 		
 	}
-	
-	public int getAforo_actual() {
-		return Aforo_actual;
-	}
-	
 	public void addPersona(Persona persona) {
 		Aforo.add(persona);
+	}
+	
+	/*
+	 * Getters y Setters
+	 */
+	public int getAforo_actual() {
+		return Aforo_actual;
 	}
 }
