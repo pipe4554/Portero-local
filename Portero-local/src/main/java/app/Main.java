f9d84414e588261c416001bdbd9f1afb894d4fcc
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-18 12:34:25 +0100

    Update Main.java

--- Portero-local/src/main/java/app/Main.java
+++ Portero-local/src/main/java/app/Main.java
@@ -1,15 +1,18 @@
 package app;
-
+/**
+ * 
+ * @author Luis Felipe Mejia 
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		Portero portero = new Portero();
-		System.out.println();
+		System.out.println("\n*Inicio del programa*");
 		for (int i = 0; i < 100; i++) {
 			String nombre = "pedro" + i;
 			Persona.CrearHiloPersona(nombre , portero);
 		}
-		//fallo en los limites de los Arrays
 	}
 
 }
