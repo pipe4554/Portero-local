d97d6272f05ead5c202b68a75a2cc648e73d5fd6
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-18 11:06:30 +0100

    Update Main.java

--- Portero-local/src/main/java/app/Main.java
+++ Portero-local/src/main/java/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		Portero portero = new Portero();
-		
+		System.out.println();
 		for (int i = 0; i < 100; i++) {
 			String nombre = "pedro" + i;
 			Persona.CrearHiloPersona(nombre , portero);
