b4fe6f244ebf91c6b9620383e8aa8026333e3f5a
Luis Felipe Mejia Aguirre <71395469+pipe4554@users.noreply.github.com>
2022-02-18 11:06:26 +0100

    Update Portero.java

--- Portero-local/src/main/java/app/Portero.java
+++ Portero-local/src/main/java/app/Portero.java
@@ -3,27 +3,69 @@ package app;
 import java.util.concurrent.Semaphore;
 
 public class Portero {
-	private static final int MAX_AVAILABLE = Edificio.getAFORO_MAX(); //tamaÃ±o maximo del aforo
+	private static final int MAX_AVAILABLE = Edificio.getAFORO_MAX(); // tamaÃ±o maximo del aforo
 	private final Semaphore available = new Semaphore(MAX_AVAILABLE, true); // el semaforo
-
-	
-	protected Edificio edificio = new Edificio();
-
+	protected Edificio edificio = new Edificio(); // Variable que almacena las personas
+	/*
+	 * Vector de booleanos que almecena el estado de la lista de las personas del
+	 * aforo
+	 */
 	protected boolean[] used = new boolean[MAX_AVAILABLE];
 
-	public Persona getItem() throws InterruptedException {
-		available.acquire(); //el hilo "pide turno"
-		return getNextAvailableItem();
-	}
-
+	/*
+	 * Metodos de entrada de personas
+	 */
+	/**
+	 * Metodo que pide permiso al semaforo para introducir una persona posteriormente.
+	 * 
+	 * @param persona que entra
+	 * @throws InterruptedException
+	 */
 	public void putItem(Persona persona) throws InterruptedException {
-		available.acquire(); //el hilo "pide turno"
+		available.acquire(); // el hilo "pide turno"
 		if (markAsUnused(persona)) {
 			available.release();
 		}
 	}
-	
-	
+
+	/**
+	 * Metodo que introduce una persona en la cola
+	 * 
+	 * @param persona
+	 * @return valor del vector de
+	 */
+	protected synchronized boolean markAsUnused(Persona persona) {
+		for (int i = 0; i < MAX_AVAILABLE; ++i) {
+			if (!used[i]) {
+				edificio.addPersona(persona);
+				edificio.entra();
+				System.out.println("Aforo actual: " + edificio.getAforo_actual());
+				return true;
+			}
+
+		}
+		return false;
+
+	}
+
+	/**
+	 * Metodo para obtener el permiso del semaforo para sacar una persona que se
+	 * encuentra en la cola.
+	 * 
+	 * @return metodo para sacar persona
+	 * @throws InterruptedException
+	 */
+	public Persona getItem() throws InterruptedException {
+		available.acquire(); // el hilo "pide turno"
+		return getNextAvailableItem();
+	}
+
+	/**
+	 * Metodo que saca una persona de la cola a la vez que actualiza el estado del
+	 * vector de booleanos
+	 * 
+	 * @return Persona que sale
+	 */
 	protected synchronized Persona getNextAvailableItem() {
 		for (int i = 0; i < MAX_AVAILABLE; ++i) {
 			if (!used[i]) {
@@ -34,20 +76,7 @@ public class Portero {
 			}
 		}
 		Persona persona = new Persona("error");
-		return persona; // not reached
+		return persona; // Persona error
 	}
 
-	protected synchronized boolean markAsUnused(Persona persona) {
-		for (int i = 0; i < MAX_AVAILABLE; ++i) {
-			if (!used[i]) {
-				edificio.addPersona(persona);
-				edificio.entra();
-				System.out.println("Aforo actual: " + edificio.getAforo_actual()); 
-				return true;
-			}
-
-		}
-		return false;
-
-	}
 }
